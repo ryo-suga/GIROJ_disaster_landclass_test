111c97905830697f2e240be59b0a71f4811a1229
ryo-sugawara <ryo-sugawara@secom.co.jp>
2023-04-28 18:41:07 +0900

    水災等地用のアップロード用ファイル一式追加

--- MACRO/等値データアップロードファイル一式/バッチ/[検証]等地データアップロード.bat
+++ MACRO/等値データアップロードファイル一式/バッチ/[検証]等地データアップロード.bat
@@ -3,7 +3,7 @@ rem **************************************************************************
 rem クライアント環境に合わせて以下を変更
 rem (例)
 rem set MACRO="C:\Program Files (x86)\teraterm\ttpmacro.exe"
-rem set RUN_UPLOAD_SHELL="C:\Program Files (x86)\teraterm\GIROJ\runUploadShell.ttl"
+rem set RUN_UPLOAD_SHELL="C:\Program Files (x86)\teraterm\GIROJ_LANDCLASS\runUploadShell.ttl"
 
 rem ttpmacro.exeの場所
 set MACRO="ttpmacro.exeファイルの場所(TeraTermインストールフォルダ直下にあります)"
@@ -11,7 +11,7 @@ rem アップロードマクロの設置場所
 set RUN_UPLOAD_SHELL="型式データアップロードマクロ(runUploadShell.ttl)の場所"
 rem **************************************************************************
 
-set CONFIG=config_hon.ttl
+set CONFIG=config_test.ttl
 
 %MACRO% %RUN_UPLOAD_SHELL% %CONFIG%
 
